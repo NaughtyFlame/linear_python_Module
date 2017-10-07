7ca8187e0d1a595b5182fc9697c8bc6aa5cc3826
SHI <andrewattention@gmail.com>
2017-10-07 22:24:16 +0200

    Feature:cross

--- vector.py
+++ vector.py
@@ -7,6 +7,8 @@ getcontext().prec = 30
 class Vector(object):
 
     CANNOT_NORMALIZE_ZERO_VECTOR_MSG = 'Cannot normalize the zero vector'
+    NO_UNIQUE_PARALLEL_COMPONENT_MSG = 'No unique parallel component'
+    ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG = 'Only defined in two three dims'
 
     def __init__(self, coordinates):
         try:
@@ -20,6 +22,21 @@ class Vector(object):
 
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
+
+
+    def plus(self, v):
+        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+
+    def minus(self, v):
+        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+
+    def times_scalar(self, c):
+        new_coordinates = [Decimal(c)*x for x in self.coordinates]
+        return Vector(new_coordinates)
 
 
     def magnitude(self):
@@ -70,19 +87,58 @@ class Vector(object):
                 raise e
 
 
-    def plus(self, v):
-        new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
-        return Vector(new_coordinates)
+    def component_orthogonal_to(self, basis):
+        try:
+            projection = self.component_parallet_to(basis)
+            return self.minus(projection)
+
+        except Exception as e:
+            if str(e) == self.NO_UNIQUE_PARALLEL_COMPONENT_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
+
+    def component_parallet_to(self, basis):
+        try:
+            u = basis.normalized()
+            weight = self.dot(u)
+            return u.times_scalar(weight)
+        except Exception as e:
+            if str(e) == self.CANNOT_NORMALIZE_ZERO_VECTOR_MSG:
+                raise Exception(self.NO_UNIQUE_PARALLEL_COMPONENT_MSG)
+            else:
+                raise e
 
 
-    def minus(self, v):
-        new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
-        return Vector(new_coordinates)
+    def area_of_triangle_with(self, v):
+        return self.area_of_parallelogram_with(v) / Decimal('2.0')
 
 
-    def times_scalar(self, c):
-        new_coordinates = [Decimal(c)*x for x in self.coordinates]
-        return Vector(new_coordinates)
+    def area_of_parallelogram_with(self, v):
+        cross_product = self.cross(v)
+        return cross_product.magnitude()
+
+
+    def cross(self, v):
+        try:
+            x_1, y_1, z_1 = self.coordinates
+            x_2, y_2, z_2 = v.coordinates
+            new_coordinates = [   y_1*z_2 - y_2*z_1 ,
+                                -(x_1*z_2 - x_2*z_1),
+                                  x_1*y_2 - x_2*y_1   ]
+            return Vector(new_coordinates)
+
+        except ValueError as e:
+            msg = str(e)
+            if msg == 'need more than 2 values to unpack':
+                self_embedded_in_R3 = Vector(self.coordinates + ('0',))
+                v_embedded_in_R3 = Vector(v.coordinates + ('0',))
+                return self_embedded_in_R3.cross(v_embedded_in_R3)
+            elif (msg == 'too many values to unpack' or
+                  msg == 'need more than 1 value to unpack'):
+                  raise Exception(self.ONLY_DEFINED_IN_TWO_THREE_DIMS_MSG)
+            else:
+                raise e
 
 
     def __str__(self):
